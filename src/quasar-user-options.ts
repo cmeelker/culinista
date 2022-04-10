36844cb8ed944d1c0f928643f478dfe164b87152
Christa <christa@infi.nl>
2022-04-10 13:48:19 +0200

    debugging

--- src/quasar-user-options.ts
+++ src/quasar-user-options.ts
@@ -1,12 +1,12 @@
 import "./styles/quasar.scss";
-import iconSet from "quasar/icon-set/ionicons-v4.js";
-import lang from "quasar/lang/nl.js";
+//import iconSet from "quasar/icon-set/ionicons-v4.js";
+//import lang from "quasar/lang/nl.js";
 import "@quasar/extras/roboto-font/roboto-font.css";
 import "@quasar/extras/ionicons-v4/ionicons-v4.css";
 
 export default {
   config: {},
   plugins: {},
-  lang: lang,
-  iconSet: iconSet,
+  //lang: lang,
+  //iconSet: iconSet,
 };
