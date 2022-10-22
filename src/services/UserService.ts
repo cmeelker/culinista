e20de833acef70c85147788a33720fbbd3200304
Christa <christameelker@gmail.com>
2022-10-22 20:32:32 +0200

    Added catch statement

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,7 +1,14 @@
 import axios from "@/services/ApiClient";
 
 export async function getUserName(userId: string) {
-  const { data } = await axios.get(`/User?userId=${userId}`);
+  let userName = "Joop";
 
-  return data;
+  try {
+    const { data } = await axios.get(`/User?userId=${userId}`);
+    userName = data;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return userName;
 }
