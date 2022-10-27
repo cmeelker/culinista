2887710ec1ced69bc6af0ff67bb78443116afe37
Christa <christameelker@gmail.com>
2022-10-28 01:04:36 +0200

    Removed loading state from favourites

--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -1,7 +1,7 @@
 <template>
   <RecipeSearch
     :recipes="data"
-    :is-loading="isLoading"
+    :is-loading="false"
     :error="(error as string)"
   />
 </template>
@@ -9,23 +9,18 @@
 <script lang="ts" setup>
 import { fetchFavorites } from "@/services/FavoriteService";
 import { useAuth0 } from "@auth0/auth0-vue";
-import { ref, watch } from "vue";
+import { watch } from "vue";
 import { useQuery, useQueryClient } from "vue-query";
 import RecipeSearch from "../components/recipe/RecipeSearch.vue";
 
 const { user } = useAuth0();
 const queryClient = useQueryClient();
-const isLoading = ref(true);
 
 const { data, error } = useQuery("favorites", () => {
-  if (user.value.sub) {
-    return fetchFavorites(user.value.sub);
-  }
+  return fetchFavorites(user.value.sub as string);
 });
 
 watch(user, () => {
-  queryClient
-    .invalidateQueries("favorites")
-    .then(() => (isLoading.value = false));
+  queryClient.invalidateQueries("favorites");
 });
 </script>
